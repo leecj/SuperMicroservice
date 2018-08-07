19698cb3054b3b8cb3e812612727c2035c3dea1d
李程俊 <lcjqq99@live.cn>
2018-08-07 10:21:23 +0800

    ribbon hystrix

--- service-ribbon/src/main/java/com/joinus/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/joinus/serviceribbon/service/HelloService.java
@@ -1,4 +1,6 @@
 package com.joinus.serviceribbon.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,9 +11,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVICE-CLIENT/hi?name="+name,String.class);
+        return restTemplate.getForObject("http://SERVICE-CLIENT/hi?name=" + name, String.class);
     }
 
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
 
 }
