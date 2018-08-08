fefde90110c76d5e32c28e5aee113e20405ad09a
李程俊 <lcjqq99@live.cn>
2018-08-08 10:09:17 +0800

    断路器监控

--- eureka-client/src/main/java/com/joinus/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/joinus/eurekaclient/EurekaClientApplication.java
@@ -1,17 +1,33 @@
 package com.joinus.eurekaclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaClientApplication {
+
+    /**
+     * 访问地址 http://localhost:8762/actuator/hystrix.stream
+     * @param args
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
@@ -21,7 +37,12 @@ public class EurekaClientApplication {
     String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "lee") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 }
